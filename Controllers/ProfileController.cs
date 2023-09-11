6d29363fd809c69f0472006a6a93bd34646631cb
Ojelcodes <91084655+Ojelcodes@users.noreply.github.com>
2023-09-11 08:32:37 +0100

    first commit

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace BackendStageOneTask.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetProfile([FromQuery] string slackName, [FromQuery] string track)
+        {
+            var Current_day = DateTime.UtcNow/*.AddHours()*/;
+            var gitHubFileUrl = $"https://github.com/Ojelcode";
+
+            // Get the GitHub URL of the full source code
+            var gitHubSourceUrl = "https://github.com/Ojelcode/YourRepo";
+
+            var profile = new
+            {
+                SlackName = slackName,
+                CurrentDayOfWeek = Current_day.DayOfWeek.ToString(),
+                CurrentUTCTime = Current_day,
+                Track = track,
+                GitHubFileUrl = gitHubFileUrl,
+                GitHubSourceUrl = gitHubSourceUrl
+            };
+
+            return Json(profile);
+
+
+        }
+
+    }
+}
+
+
+
+
